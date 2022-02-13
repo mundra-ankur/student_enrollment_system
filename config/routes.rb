8a5f83e980ff458a444129c90ebbc94814248a64
amundra <amundra@ncsu.edu>
2022-02-13 14:20:47 -0500

    Student Login Signup with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  get 'instructors/home'
+  # Defines the root path route ("/")
   root 'home#index'
   get 'home/about'
+  get 'instructors/home'
+  get 'students/home'
 
   devise_for :admins, controllers: { registrations: 'admins/registrations' }
   devise_for :instructors, controllers: { registrations: 'instructors/registrations' }
+  devise_for :students, controllers: { registrations: 'students/registrations' }
 
   get '/admins', to: 'admins#home', as: :admin_root
+  get '/students', to: 'students#home', as: :student_root
   get '/instructors', to: 'instructors#home', as: :instructor_root
 end
