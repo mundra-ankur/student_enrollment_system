458657839d9d126a35d886de4ee9239abfcb14af
sbabu <sbabu@ncsu.edu>
2022-02-14 22:09:46 -0500

    student currently registered courses

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,6 +5,8 @@ class StudentsController < ApplicationController
     if current_student.id != nil
       @courses = Course.all
       @student = Student.find(current_student.id)
+      @enrolledcourses = Course.where(code: Enroll.where(student_id: current_student.id).pluck(:course_id))
+
     else
       @courses = nil
     end
