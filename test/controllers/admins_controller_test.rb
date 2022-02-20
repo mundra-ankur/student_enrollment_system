3d2163c3b2deb008e8736a09d445f060b204e704
sbabu <sbabu@ncsu.edu>
2022-02-19 23:50:24 -0500

    added tests for admin

--- test/controllers/admins_controller_test.rb
+++ test/controllers/admins_controller_test.rb
@@ -1,8 +1,34 @@
-require "test_helper"
+require "../test_helper"
 
 class AdminsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    get admin_session_path
+    sign_in admins(:one)
+  end
+
   test "should get index" do
-    get admins_index_url
+    get admin_root_path
     assert_response :success
   end
+
+  test "should get instructors" do
+    get admin_instructors_path
+    assert assigns(:instructors)
+    assert_response :success
+  end
+
+  test "should get courses" do
+    get admin_courses_path
+    assert assigns(:courses)
+    assert_response :success
+  end
+
+  test "should get students" do
+    get admin_students_path
+    assert assigns(:students)
+    assert_response :success
+  end
+
 end
